0d4f41bbd9ebcb068b3e4e3ba8445d5c555a3ec2
co18328 <42895800+co18328@users.noreply.github.com>
2018-09-03 21:22:49 +0530

    Add files via upload

---
+++ jasmine.c
@@ -0,0 +1,185 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+	int ans;
+	printf("\t\t\tWelcome to KBC 2018");
+	printf("\n\n Q1) WhAt was the capital of Rama? \n  1.Ayodhya \t 2.Lanka \t 3.New Delhi \t 4.Ram Nagar \n");
+	scanf("%d",&sol);
+	if(sol==1)
+	{
+		printf("Correct Answer\nYou won Rs. 5000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Ayodhya \n Final money won Rs. 0 \n Game over \n");
+		exit(0);
+	}
+	printf("\n Q2) Who was the first President of India? \n 1.C Rajgopalachari \t 2.Pandit Nehru \t 3.Rajendra Prasad \t 4.B R Ambedkar \n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs. 10,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Rajendra Prasad \n Final money won Rs. 5,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q3) Which is the 5th planet of the solar system? \n 1.Jupitar \t 2.Mars \t 3.Uranus \t 4.Saturn \n");
+	scanf("%d",&sol);
+	if(sol==1)
+	{
+		printf("Correct Answer\nYou won Rs. 20,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Jupitar \n Final money won Rs. 10,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q4) Who was not the Railway Minister? \n 1.Mamatha \t 2.Lal Bahadur \t 3.Geetha Mukharjee \t 4.George Fernandis \n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs.40,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Geetha Fernandis \n Final money won Rs. 20,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q5) Whos is tha eldest son of Kunthi? \n 1.Karna \t 2.Arjuna \t 3.Yudhistira \t 4.Bhima\n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs.80,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Karna \n Final money won Rs. 40,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q6) When did Second world war end? \n 1.1939 \t 2.1919 \t 3.1945 \t 4.1947\n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs.1,60,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.1945 \n Final money won Rs. 80,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q6) Who heads Reserve Bank of India? \n 1.Finance Minister \t 2.Commissioner \t 3.Chief Bank Manager \t 4.Governor\n");
+	scanf("%d",&sol);
+	if(sol==4)
+	{
+		printf("Correct Answer\nYou won RS.3,20,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 4.Governor \n Final money won Rs. 1,60,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q7) In which direction does a muslim pray? \n 1.Mecca's Direction \t 2.East \t 3.Nearby Mosque's Direction \t 4.South\n");
+	scanf("%d",&sol);
+	if(sol==2)
+	{
+		printf("Correct Answer\nYou won Rs.6,40,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Mecca's Direction \n Final money won Rs. 3,20,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q8) Which is the currency of Japan? \n 1.Japanese Dollar \t 2.Zen \t 3.Jen \t 4.Yen\n");
+	scanf("%d",&sol);
+	if(sol==4)
+	{
+		printf("Correct Answer\nYou won Rs.12,50,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 4.Yen \n Final money won Rs. 6,40,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q9) Whicj of these is not an ocean? \n 1.Arabian \t 2.Indian \t 3.Pacific \t 4.Atlantic\n");
+	scanf("%d",&sol);
+	if(sol==1)
+	{
+		printf("Correct Answer\nYou won Rs.25,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Arabian \n Final money won Rs. 5000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q10) Where is the Kohinoor diamond? \n 1.Persia \t 2.USA \t 3.India \t 4.England\n");
+	scanf("%d",&sol);
+	if(sol==4)
+	{
+		printf("Correct Answer\nYou won Rs.50,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 4.England \n Final money won Rs. 25,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q11) In which continent is Atlas Mountain Range? \n 1.Africa \t 2.Asia \t 3.Europe \t 4.Antartica\n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs.1,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Africa \n Final money won Rs. 50,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q12) Where is the nalanda university \n 1.Punjab \t 2.Madhya Pradesh \t 3.Bihar \t 4.Utter Pradesh\n");
+	scanf("%d",&sol);
+	if(sol==3)
+	{
+		printf("Correct Answer\nYou won Rs.2,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Bihar \n Final money won Rs. 1,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q13) Which is the largest mammal? \n 1.Elephant \t 2.Hippo \t 3.Blue Whale \t 4.Giraffe\n");
+	scanf("%d",&sol);
+	if(sol==3.)
+	{
+		printf("Correct Answer\nYou won Rs.3,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 3.Blue Whale \n Final money won Rs. 2,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q14) Who is the father of Ghatodhgaja in Mahabharath? \n 1.Bhima \t 2.Hidimbha \t 3.Arjuna \t 4.Kumbhakarna\n");
+	scanf("%d",&sol);
+	if(sol==1)
+	{
+		printf("Correct Answer\nYou won Rs.5,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Bhima \n Final money won Rs. 3,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	printf("\n Q15) Who is the current Foriegn Minister of India? \n 1.Sushma Swaraj \t 2.Mamata Banerjee \t 3.Smiriti Irani \t 4.Mayawat\n");
+	scanf("%d",&sol);
+	if(sol==1)
+	{
+		printf("Correct Answer\nYou won Rs.7,00,00,000\n");
+	}
+	else
+	{
+		printf("Wrong Answer \n The right answer is 1.Sushma Swaraj \n Final money won Rs. 5,00,00,000 \n Game over\n");
+		exit(0);
+	}
+	return 0;
+}
